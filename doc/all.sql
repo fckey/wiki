2a30389db71e6d016c0bc2c514718695736fd053
william Fang <sl.fang@qq.com>
2022-06-07 22:20:01 +0800

    3.5 集成mybatis generator,生成demo代码

--- doc/all.sql
+++ doc/all.sql
@@ -1,7 +1,16 @@
 drop table if exists `test`;
-create table `test`(
-    `id` bigint not null comment 'id',
-    'name' varchar(50) comment '名称',
-    'password' varchar(50) comment '密码',
-    primary key('id')
+create table `test` (
+   `id` bigint not null comment 'id',
+   `name` varchar(50) comment '名称',
+   `password` varchar(50) comment '密码',
+   primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='测试';
+
+drop table if exists `demo`;
+create table `demo` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='测试';
+
+insert into `demo` (id, name) values (1, '测试');
