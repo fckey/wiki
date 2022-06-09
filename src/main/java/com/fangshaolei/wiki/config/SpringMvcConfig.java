b357ee960a4cb76462f63ada40d0f75f641865fa
william Fang <sl.fang@qq.com>
2022-06-09 20:11:19 +0800

    5.8 增加拦截器，打印接口耗时

---
+++ src/main/java/com/fangshaolei/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,28 @@
+package com.fangshaolei.wiki.config;
+
+import com.fangshaolei.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @author fangshaolei
+ * @version 1.0.0
+ * @ClassName SpringMvcConfig
+ * @Description
+ * @createTime 2022/06/09 20:09
+ **/
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
