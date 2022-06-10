b6e4caa71fa94744bfbbd381757b1509c8e21477
william Fang <sl.fang@qq.com>
2022-06-10 10:23:58 +0800

    6.6 电子书管理页面前后端分页整合完成

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -86,13 +86,19 @@ export default defineComponent({
      **/
     const handleQuery = (params: any) => {
       loading.value = true;
-      axios.get("/ebook/list", params).then((response) => {
+      axios.get("/ebook/list", {
+          params:{
+            page: params.page,
+            size: params.size
+          }
+      }).then((response) => {
         loading.value = false;
         const data = response.data;
-        ebooks.value = data.content;
+        ebooks.value = data.content.list;
 
         // 重置分页按钮
         pagination.value.current = params.page;
+        pagination.value.total = data.content.total;
       });
     };
 
@@ -108,7 +114,10 @@ export default defineComponent({
     };
 
     onMounted(() => {
-      handleQuery({});
+      handleQuery({
+          page: 1,
+          size: pagination.value.pageSize,
+      });
     });
 
     return {
