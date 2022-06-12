c75f2b43d02b059394a402059d737560c61ea641
william Fang <sl.fang@qq.com>
2022-06-12 21:58:41 +0800

    6.12 增加按名字查询电子书功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -5,9 +5,22 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <p>
-        <a-button type="primary" @click="add()" size="large">
-          新增
-        </a-button>
+        <a-form layout="inline" :model="param">
+          <a-form-item>
+            <a-input v-model:value="param.name" placeholder="名称">
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="handleQuery({page: 1, size: pagination.pageSize})">
+              查询
+            </a-button>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="add()">
+              新增
+            </a-button>
+          </a-form-item>
+        </a-form>
       </p>
       <a-table
           :columns="columns"
@@ -33,11 +46,9 @@
                 @confirm="handleDelete(record.id)"
             >
               <a-button type="danger">
-              删除
-            </a-button>
+                删除
+              </a-button>
             </a-popconfirm>
-
-
           </a-space>
         </template>
       </a-table>
@@ -78,6 +89,8 @@ import {message} from 'ant-design-vue';
 export default defineComponent({
   name: 'AdminEbook',
   setup() {
+    const param = ref();
+    param.value = {};
     const ebooks = ref();
     const pagination = ref({
       current: 1,
@@ -131,17 +144,18 @@ export default defineComponent({
       axios.get("/ebook/list", {
         params: {
           page: params.page,
-          size: params.size
+          size: params.size,
+          name: param.value.name
         }
       }).then((response) => {
         loading.value = false;
         const data = response.data;
-        if(data.success){
+        if (data.success) {
           ebooks.value = data.content.list;
           // 重置分页按钮
           pagination.value.current = params.page;
           pagination.value.total = data.content.total;
-        }else{
+        } else {
           message.error(data.message);
         }
       });
@@ -174,7 +188,7 @@ export default defineComponent({
             page: pagination.value.current,
             size: pagination.value.pageSize,
           });
-        }else{
+        } else {
           message.error(data.message);
         }
       });
@@ -196,7 +210,7 @@ export default defineComponent({
       ebook.value = {};
     };
 
-    const  handleDelete = (id: number) => {
+    const handleDelete = (id: number) => {
       axios.delete("/ebook/delete/" + id).then((response) => {
         const data = response.data; // data = commonResp
         if (data.success) {
@@ -217,11 +231,13 @@ export default defineComponent({
     });
 
     return {
+      param,
       ebooks,
       pagination,
       columns,
       loading,
       handleTableChange,
+      handleQuery,
       edit,
       add,
       ebook,
