1895af432f80af0f9a294a340eb3087967bb01af
william Fang <sl.fang@qq.com>
2022-06-20 14:49:19 +0800

    7-7 首页显示分页菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -5,33 +5,24 @@
           mode="inline"
           v-model:openKeys="openKeys"
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span><user-outlined />  subnav 1</span>
+
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined/>
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined/>{{ item.name }}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span><laptop-outlined />subnav 2</span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-            <span><notification-outlined />subnav 3</span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined/>
+            <span>{{ child.name }}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -43,7 +34,7 @@
           <a-list-item key="item.name">
             <template #actions>
               <span v-for="{ type, text } in actions" :key="type">
-                <component v-bind:is="type" style="margin-right: 8px" />
+                <component v-bind:is="type" style="margin-right: 8px"/>
                 {{ text }}
               </span>
             </template>
@@ -51,7 +42,9 @@
               <template #title>
                 <a :href="item.href">{{ item.name }}</a>
               </template>
-              <template #avatar><a-avatar :src="item.cover" /></template>
+              <template #avatar>
+                <a-avatar :src="item.cover"/>
+              </template>
             </a-list-item-meta>
           </a-list-item>
         </template>
@@ -61,8 +54,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted,ref,reactive,toRef } from 'vue';
+import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
+import {message} from 'ant-design-vue';
+import {Tool} from "@/util/tool";
 
 // const listData: any = [];
 //
@@ -80,12 +75,39 @@ import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
-  setup(){
+  setup() {
     const ebooks = ref();
-    const ebooks1 = reactive({books: []});
 
-    onMounted(() =>{
-      axios.get("/ebook/list",{
+    // const ebooks1 = reactive({books: []});
+
+    const level1 = ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = () => {
+      console.log("menu click")
+    };
+
+    onMounted(() => {
+      handleQueryCategory();
+      axios.get("/ebook/list", {
         params: {
           page: 1,
           size: 1000
@@ -97,7 +119,7 @@ export default defineComponent({
       });
     })
 
-    return{
+    return {
       ebooks,
       // ebooks2:toRef(ebooks1, "books"),
       // listData,
@@ -112,6 +134,8 @@ export default defineComponent({
         {type: 'LikeOutlined', text: '156'},
         {type: 'MessageOutlined', text: '2'}
       ],
+      handleClick,
+      level1,
     }
   }
 
