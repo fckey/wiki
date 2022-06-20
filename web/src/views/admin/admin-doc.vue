5d8f0fcf06c7c16376a6eca5545b9e1fde96e0bc
william Fang <sl.fang@qq.com>
2022-06-20 19:36:28 +0800

    8-5 增加页面参数传递完成新增文档功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -84,10 +84,12 @@ import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
 import {message} from 'ant-design-vue';
 import {Tool} from "@/util/tool";
+import {useRoute} from "vue-router";
 
 export default defineComponent({
   name: 'Admindoc',
   setup() {
+    const route = useRoute();
     const param = ref();
     param.value = {};
     const docs = ref();
@@ -218,7 +220,9 @@ export default defineComponent({
      */
     const add = () => {
       modalVisible.value = true;
-      doc.value = {};
+      doc.value = {
+        ebookId: route.query.ebookId
+      };
       treeSelectData.value = Tool.copy(level1.value);
 
       // 为选择树添加一个"无"
