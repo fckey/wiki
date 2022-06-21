3e087a1ef18e12e6b52ae362af6552b23a430e2a
william Fang <sl.fang@qq.com>
2022-06-21 09:01:08 +0800

    8-7 集成富文本编辑器wangEditor@4.6.3，在文档中显示富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -74,6 +74,9 @@
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort"/>
       </a-form-item>
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
 
   </a-modal>
@@ -86,6 +89,9 @@ import {message, Modal} from 'ant-design-vue';
 import {Tool} from "@/util/tool";
 import {useRoute} from "vue-router";
 import ExclamationCircleOutlined from "@ant-design/icons-vue/ExclamationCircleOutlined";
+import 'mavon-editor/dist/css/index.css'
+import E from 'wangeditor'
+
 
 export default defineComponent({
   name: 'Admindoc',
@@ -157,6 +163,7 @@ export default defineComponent({
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+    const editor = new E("#content");
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/doc/save", doc.value).then((response) => {
@@ -250,6 +257,9 @@ export default defineComponent({
       setDisable(treeSelectData.value, record.id);
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function () {
+        editor.create();
+      }, 100);
     };
 
     /**
@@ -264,6 +274,10 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+
+      setTimeout(function () {
+        editor.create();
+      },100);
     };
 
     const handleDelete = (id: number) => {
