304c3488491b06d74194a0bbb54d03e4bc25f0b0
william Fang <sl.fang@qq.com>
2022-06-21 09:37:03 +0800

    8-9 让表格树默认展开

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -21,12 +21,14 @@
             </a-form>
           </p>
           <a-table
+              v-if="level1.length > 0"
               :columns="columns"
               :row-key="record => record.id"
               :data-source="level1"
               :loading="loading"
               :pagination="false"
               size="small"
+              :defaultExpandAllRows="true"
           >
             <template #name="{ text, record }">
               {{record.sort}} {{text}}
@@ -142,6 +144,7 @@ export default defineComponent({
      * }]
      */
     const level1 = ref(); // 一级分类树，children属性就是二级分类
+    level1.value = [];
     /**
      * 数据查询
      **/
