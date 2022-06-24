3e9eb9614759d6a5c6d3ab3025cdf0326b422efd
william Fang <sl.fang@qq.com>
2022-06-24 09:13:10 +0800

    9-8 前后端调试成功，完成基本的登录功能

--- src/main/java/com/fangshaolei/wiki/req/UserLoginReq.java
+++ src/main/java/com/fangshaolei/wiki/req/UserLoginReq.java
@@ -1,5 +1,6 @@
 package com.fangshaolei.wiki.req;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
@@ -11,10 +12,10 @@ import javax.validation.constraints.Pattern;
  * @createTime 2022/06/24 9:06
  **/
 public class UserLoginReq {
-    @NotNull(message = "【用户名】不能为空")
+    @NotEmpty(message = "【用户名】不能为空")
     private String loginName;
 
-    @NotNull(message = "【密码】不能为空")
+    @NotEmpty(message = "【密码】不能为空")
     // @Length(min = 6, max = 20, message = "【密码】6~20位")
     @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,32}$", message = "【密码】规则不正确")
     private String password;
