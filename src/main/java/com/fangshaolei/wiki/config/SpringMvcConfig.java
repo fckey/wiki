33439a7c8ca7cb2cb15778aef011a0e97bc1c158
william Fang <sl.fang@qq.com>
2022-06-24 10:40:09 +0800

    9-11 前后端增加登录拦截 修复BUG：前后端存储token，统一用String类型

--- src/main/java/com/fangshaolei/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/fangshaolei/wiki/config/SpringMvcConfig.java
@@ -1,7 +1,11 @@
 package com.fangshaolei.wiki.config;
 
+import com.fangshaolei.wiki.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 /**
  * @author fangshaolei
@@ -13,12 +17,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-//    @Resource
-//    private LogInterceptor logInterceptor;
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-//    }
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/find-content/**"
+                );
+    }
 }
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -6,25 +6,31 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
 import axios from 'axios';
+import {Tool} from "@/util/tool";
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
 /**
  * axios拦截器
  */
-    axios.interceptors.request.use(function (config) {
-          console.log('请求参数：', config);
-          return config;
-        }, error => {
-          return Promise.reject(error);
-        });
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    const token = store.state.user.token;
+    if (Tool.isNotEmpty(token)) {
+        config.headers.token = token;
+        console.log("请求headers增加token:", token);
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
 axios.interceptors.response.use(function (response) {
-      console.log('返回结果：', response);
-      return response;
-    }, error => {
-      console.log('返回错误：', error);
-      return Promise.reject(error);
-    });
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
 
 
 const app = createApp(App);
@@ -32,7 +38,7 @@ app.use(store).use(router).use(Antd).mount('#app');
 
 // 全局使用图标
 const icons: any = Icons;
-for(const i in icons){
+for (const i in icons) {
     app.component(i, icons[i]);
 }
 
