67140d9442f2e4cf574b092bf6cd83da7a4c00e5
william Fang <sl.fang@qq.com>
2022-06-24 10:06:39 +0800

    9-9 使用vuex保存用户信息，方便其它组件获取登录信息。遗留问题，刷新后值没有了

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,12 +1,20 @@
 <template>
   <a-layout-footer style="text-align: center">
-    LSWiki @2022 Created by fangshaolei
+    LSWiki @2022 Created by fangshaolei,welcome：{{ user.name }}
   </a-layout-footer>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent, computed} from 'vue';
+import store from "@/store";
 
 export default defineComponent({
-  name: 'the-footer'
+  name: 'the-footer',
+  setup() {
+    const user = computed(() => store.state.user);
+
+    return {
+      user
+    }
+  }
 });
 </script>
