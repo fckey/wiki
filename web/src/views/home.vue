d04e814293b391cc5b827a466489fe4f6ac6739e
william Fang <sl.fang@qq.com>
2022-06-24 14:22:26 +0800

    10-6 首页显示电子书信息：文档数、阅读数、点赞数

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -1,21 +1,22 @@
 <template>
   <a-layout>
-    <a-layout-sider width="200" style="background: #fff">
+    <a-layout-sider style="background: #fff" width="200">
       <a-menu
-          mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
+          mode="inline"
           @click="handleClick"
       >
         <a-menu-item key="welcome">
-          <MailOutlined />
+          <MailOutlined/>
           <span>欢迎</span>
         </a-menu-item>
         <a-sub-menu v-for="item in level1" :key="item.id">
           <template v-slot:title>
-            <span><user-outlined />{{item.name}}</span>
+            <span><user-outlined/>{{ item.name }}</span>
           </template>
           <a-menu-item v-for="child in item.children" :key="child.id">
-            <MailOutlined /><span>{{child.name}}</span>
+            <MailOutlined/>
+            <span>{{ child.name }}</span>
           </a-menu-item>
         </a-sub-menu>
       </a-menu>
@@ -23,16 +24,25 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <div class="welcome" v-show="isShowWelcome">
+      <div v-show="isShowWelcome" class="welcome">
         <h1>欢迎使用甲蛙知识库</h1>
       </div>
-      <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+      <a-list v-show="!isShowWelcome" :data-source="ebooks" :grid="{ gutter: 20, column: 3 }" item-layout="vertical"
+              size="large">
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
-              <span v-for="{ type, text } in actions" :key="type">
-                <component v-bind:is="type" style="margin-right: 8px" />
-                {{ text }}
+               <span>
+               <component v-bind:is="'FileOutlined'" style="margin-right: 8px"/>
+                {{ item.docCount }}
+             </span>
+              <span>
+                <component v-bind:is="'UserOutlined'" style="margin-right: 8px"/>
+                {{ item.viewCount }}
+              </span>
+              <span>
+                <component v-bind:is="'LikeOutlined'" style="margin-right: 8px"/>
+                {{ item.voteCount }}
               </span>
             </template>
             <a-list-item-meta :description="item.description">
@@ -41,7 +51,9 @@
                   {{ item.name }}
                 </router-link>
               </template>
-              <template #avatar><a-avatar :src="item.cover"/></template>
+              <template #avatar>
+                <a-avatar :src="item.cover"/>
+              </template>
             </a-list-item-meta>
           </a-list-item>
         </template>
@@ -51,9 +63,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
+import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
-import { message } from 'ant-design-vue';
+import {message} from 'ant-design-vue';
 import {Tool} from "@/util/tool";
 
 // const listData: any = [];
@@ -75,7 +87,7 @@ export default defineComponent({
     const ebooks = ref();
     // const ebooks1 = reactive({books: []});
 
-    const level1 =  ref();
+    const level1 = ref();
     let categorys: any;
     /**
      * 查询所有分类
@@ -140,12 +152,11 @@ export default defineComponent({
         },
         pageSize: 3,
       },
-      actions: [
-        { type: 'StarOutlined', text: '156' },
-        { type: 'LikeOutlined', text: '156' },
-        { type: 'MessageOutlined', text: '2' },
-      ],
-
+      // actions: [
+      //   {type: 'StarOutlined', text: '156'},
+      //   {type: 'LikeOutlined', text: '156'},
+      //   {type: 'MessageOutlined', text: '2'},
+      // ],
       handleClick,
       level1,
 
